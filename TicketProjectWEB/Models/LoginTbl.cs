13d7e9e8b0c2fbc7775382e5dc46d2acc93eeb0d
Aman Katyayan <aman.katyayan@NTZ-CPU-096>
2024-05-30 17:50:59 +0530

    Made minor Chabges.

--- TicketProjectWEB/Models/LoginTbl.cs
+++ TicketProjectWEB/Models/LoginTbl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TicketProjectWEB.Models;
 
@@ -7,8 +8,11 @@ public partial class LoginTbl
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Email is Required")]
     public string Email { get; set; } = null!;
 
+    [Required(ErrorMessage = "Passowrd is Required")]
+    [DataType(DataType.Password)]
     public string Password { get; set; } = null!;
 
     public string? Salt { get; set; }
